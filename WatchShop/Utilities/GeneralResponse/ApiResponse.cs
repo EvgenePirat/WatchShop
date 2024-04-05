0e3b92fb4f805e8262a66745415a8decaf88e876
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-05 14:20:48 -0700

    #1.17 Add general response for all controllers

---
+++ WatchShop/Utilities/GeneralResponse/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace WatchShop_UI.Utilities.GeneralResponse
+{
+    public class ApiResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public object Result { get; set; }
+    }
+}
