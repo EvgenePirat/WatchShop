80754f4d1ff24dce274686e7f601276451cf2b84
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-05 18:25:06 +0300

    #1.1 Create api and set options for get all brends

--- WatchShopClient/src/admin/pages/brends/BrendList.jsx
+++ WatchShopClient/src/admin/pages/brends/BrendList.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../../styles/BrendList.css'
 import { DataGrid } from '@mui/x-data-grid';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link, NavLink } from 'react-router-dom';
+import { useGetBrendsQuery } from '../../../apis/admin/brendApi';
+import { useDispatch } from 'react-redux';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 90 },
   { field: 'name', headerName: 'Name', width: 150 },
   { field: 'description', headerName: 'Description', width: 300 },
-  {
-    field: 'count',
-    headerName: 'count watch',
-    type: 'number',
-    width: 100,
-  },
   {
     field: "action",
     headerName: "Action",
@@ -31,18 +27,14 @@ const columns = [
     }
   }
 ];
-
-const rows = [
-  { id: 1, name: 'Rolex', description: 'Jon', count: 12 },
-  { id: 2, name: 'Fossil', description: 'Jon', count: 2 },
-  { id: 3, name: 'Omega', description: 'Jon', count: 3 },
-  { id: 4, name: 'Cartier', description: 'Jon', count: 45 },
-  { id: 5, name: 'Chopard', description: 'Jon', count: 7 },
-  { id: 6, name: 'Breguet', description: 'Jon', count: 0 },
-  { id: 7, name: 'Angelus', description: 'Jon', count: 2 },
-];
-
 function BrendList() {
+
+  const {data, isLoading} = useGetBrendsQuery(null)
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='brendList'>
       <div className='brendTitleContainer'>
@@ -51,7 +43,7 @@ function BrendList() {
       </div>
       <div style={{ width: '100%', alignItems: 'end'}}>
         <DataGrid
-          rows={rows}
+          rows={data.result}
           columns={columns}
           initialState={{
             pagination: {
--- WatchShopClient/src/apis/admin/brendApi.js
+++ WatchShopClient/src/apis/admin/brendApi.js
@@ -3,6 +3,28 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 const brendApi = createApi({
     reducerPath:"brendApi",
     baseQuery: fetchBaseQuery({
-        baseUrl:""
-    })
-})
+        baseUrl:"https://localhost:7103/api/brend/"
+    }),
+    tagTypes: ["Brends"],
+    endpoints: (buiilder) => ({
+        getBrends: buiilder.query({
+            query: () => ({
+                url:"all",
+                method: "GET"
+            }),
+            providesTags: ["Brends"],
+        }),
+        getBrendById: buiilder.query({
+            query: (id) => ({
+                url:`${id}`,
+                method: "GET"
+            }),
+            providesTags: ["Brends"],
+        })
+
+    }),
+});
+
+export const {useGetBrendsQuery, useGetBrendByIdQuery} = brendApi;
+
+export default brendApi;
