bac61a970b8c531f37a27ee225db26e09eed4d6a
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-05 14:10:02 -0700

    #1.2 Create Brend repository and set in general repository

---
+++ WatchShop_Core/Domain/RepositoryContracts/IBrendRepository.cs
@@ -0,0 +1,10 @@
+﻿using WatchShop_Core.Domain.Entities;
+
+namespace WatchShop_Core.Domain.RepositoryContracts
+{
+    public interface IBrendRepository : IRepositoryBase<Brend> 
+    {
+        Task<Brend?> GetByIdAsync(int id);
+        Task<Brend?> FindByBrendNameAsync(string name);
+    }
+}
---
+++ WatchShop_Core/Domain/RepositoryContracts/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace WatchShop_Core.Domain.RepositoryContracts
+{
+    public interface IUnitOfWork
+    {
+        IBrendRepository BrendRepository { get; }
+        Task SaveAsync();
+        Task<IDbContextTransaction> BeginTransactionDbContextAsync();
+    }
+}
