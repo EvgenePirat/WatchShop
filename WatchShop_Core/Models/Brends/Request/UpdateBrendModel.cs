e5ed1cf7bc0e6cdd4837a3b4a0765926def6d51c
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-05 14:14:58 -0700

    #1.8 Create models for working with brend in bussines layer

---
+++ WatchShop_Core/Models/Brends/Request/UpdateBrendModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchShop_Core.Models.Brends.Request
+{
+    public class UpdateBrendModel
+    {
+        [Required(ErrorMessage = "Name must be not null")]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
