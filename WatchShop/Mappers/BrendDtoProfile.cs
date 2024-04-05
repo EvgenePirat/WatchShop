045f210ee2548d04de76d6dd49e295552447947c
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-05 14:18:48 -0700

    #1.14 Create Mapper for working with dtos and models

---
+++ WatchShop/Mappers/BrendDtoProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WatchShop_Core.Models.Brends.Request;
+using WatchShop_Core.Models.Brends.Response;
+using WatchShop_UI.Dtos.Brends.Reequest;
+using WatchShop_UI.Dtos.Brends.Response;
+
+namespace WatchShop_UI.Mappers
+{
+    public class BrendDtoProfile : Profile
+    {
+        public BrendDtoProfile()
+        {
+            CreateMap<CreateBrendDto, CreateBrendModel>();
+            CreateMap<UpdateBrendDto, UpdateBrendModel>();
+            CreateMap<BrendModel, BrendDto>();
+        }
+    }
+}
