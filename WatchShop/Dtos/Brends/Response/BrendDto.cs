a1a6a73aa2f74d2a5b8d7e16041f8ef069a465d8
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-05 14:18:23 -0700

    #1.14 Create Dtos for working with brend

---
+++ WatchShop/Dtos/Brends/Response/BrendDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WatchShop_UI.Dtos.Brends.Response
+{
+    public class BrendDto
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
