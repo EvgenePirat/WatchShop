467efc4fe9e4c2bf3fcde8a8b0031e3b1d970836
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-05 14:12:40 -0700

    #1.4 Realize brend repository and general repository

---
+++ WatchShop_Infrastructure/Repositories/BrendRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WatchShop_Core.Domain.Entities;
+using WatchShop_Core.Domain.RepositoryContracts;
+using WatchShop_Infrastructure.DbContext;
+
+namespace WatchShop_Infrastructure.Repositories
+{
+    public class BrendRepository : RepositoryBase<Brend>, IBrendRepository
+    {
+        public BrendRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Brend?> FindByBrendNameAsync(string name)
+        {
+            return await _context.Brends.FirstOrDefaultAsync(b => b.Name.ToLower() == name.ToLower());
+        }
+
+        public async Task<Brend?> GetByIdAsync(int id)
+        {
+            return await _context.Brends.FirstOrDefaultAsync(b => b.Id == id);
+        }
+    }
+}
