cdfd7c2b6ae2e12fe8020d0b47d9624a6068a688
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-06 12:43:33 -0700

    #3.2 Create logic for get all watch characteristic

---
+++ WatchShop/Controllers/WatchController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WatchShop_UI.Dtos.Products.Response;
+using WatchShop_UI.Utilities.GeneralResponse;
+
+namespace WatchShop_UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WatchController : ControllerBase
+    {
+        private readonly ILogger<BrendController> _logger;
+
+        public WatchController(ILogger<BrendController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("characteristic")]
+        public async Task<ActionResult<ApiResponse>> GetWatchCharacteristic()
+        {
+            _logger.LogInformation("{controller}.{method} - Get, get watch characteristic, Task started",
+                nameof(WatchController), nameof(GetWatchCharacteristic));
+
+            var result = new WatchCharacters();
+
+            ApiResponse response = new ApiResponse()
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                Result = result
+            };
+
+            _logger.LogInformation("{controller}.{method} - Get, get watch characteristic, Result - Ok, Task ended",
+                nameof(WatchController), nameof(GetWatchCharacteristic));
+
+            return Ok(response);
+        }
+    }
+}
---
+++ WatchShop/Dtos/Watches/Response/WatchCharacters.cs
@@ -0,0 +1,43 @@
+﻿using WatchShop_UI.Dtos.Enums;
+
+namespace WatchShop_UI.Dtos.Products.Response
+{
+    public class WatchCharacters
+    {
+        public string[] AdditionalCharacteristicsEnum { get; } = Enum.GetNames<AdditionalCharacteristicsEnum>();
+
+        public string[] CaseDiameter { get; } = Enum.GetNames<CaseDiameter>();
+
+        public string[] CaseShape { get;  } = Enum.GetNames<CaseShape>();
+
+        public string[] ClockFaceColorEnum { get;  } = Enum.GetNames<ClockFaceColorEnum>();
+
+        public string[] CountryEnum { get; } = Enum.GetNames<CountryEnum>();
+
+        public string[] FrameColorEnum { get;} = Enum.GetNames<FrameColorEnum>();
+
+        public string[] FrameMaterialEnum { get;} = Enum.GetNames<FrameMaterialEnum>();
+
+        public string[] Gender { get; } = Enum.GetNames<Gender>();
+
+        public string[] GlassTypeEnum { get; } = Enum.GetNames<GlassTypeEnum>();
+
+        public string[] GuaranteeMonth { get; } = Enum.GetNames<GuaranteeMonth>();
+
+        public string[] IndicationKindEnum { get; } = Enum.GetNames<IndicationKindEnum>();
+
+        public string[] IndicationTypeEnum { get; } = Enum.GetNames<IndicationTypeEnum>();
+
+        public string[] MechanismTypeEnum { get; } = Enum.GetNames<MechanismTypeEnum>();
+
+        public string[] StrapEnum { get; } = Enum.GetNames<StrapEnum>();
+
+        public string[] StrapMaterialEnum { get; } = Enum.GetNames<StrapMaterialEnum>();
+
+        public string[] StyleEnum { get; } = Enum.GetNames<StyleEnum>();
+
+        public string[] TimeFormat { get; } = Enum.GetNames<TimeFormat>();
+
+        public string[] WaterResistance { get; } = Enum.GetNames<WaterResistance>();
+    }
+}
