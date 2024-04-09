ca7ef8f4c4e2c82332537dd4c44c1feb125be0e3
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-09 07:31:47 +0300

    #3.28 Registration new exception in middleware

--- WatchShop/Utilities/Middleware/ExceptionsHandlers/ExceptionHandlerMiddleware.cs
+++ WatchShop/Utilities/Middleware/ExceptionsHandlers/ExceptionHandlerMiddleware.cs
@@ -33,6 +33,7 @@ namespace WatchShop_UI.Utilities.Middleware.ExceptionsHandlers
             var response = exception switch
             {
                 BrendArgumentException _ => new ExceptionResponse(HttpStatusCode.BadRequest, exception.Message),
+                WatchArgumentException _ => new ExceptionResponse(HttpStatusCode.BadRequest, exception.Message),
                 ApplicationException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Application exception occurred."),
                 KeyNotFoundException _ => new ExceptionResponse(HttpStatusCode.NotFound, "The request key not found."),
                 UnauthorizedAccessException _ => new ExceptionResponse(HttpStatusCode.Unauthorized, "Unauthorized."),
