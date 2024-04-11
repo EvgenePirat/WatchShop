980323cee97da90a75cd47c2bce1ffd5ea069a80
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:32:48 +0300

    #5.6 Create auth mapper for working with auth logic

---
+++ WatchShop/Mappers/AuthDtoProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WatchShop_Core.Models.Auth.Request;
+using WatchShop_Core.Models.Auth.Response;
+using WatchShop_UI.Dtos.Auth.Request;
+using WatchShop_UI.Dtos.Auth.Response;
+
+namespace WatchShop_UI.Mappers
+{
+    public class AuthDtoProfile : Profile
+    {
+        public AuthDtoProfile()
+        {
+            CreateMap<LoginDto, LoginModel>();
+            CreateMap<RegisterDto, RegisterModel>();
+            CreateMap<AuthResponseModel, AuthResponseDto>();
+            CreateMap<RegisterResponseModel, RegisterResponseDto>();
+        }
+    }
+}
