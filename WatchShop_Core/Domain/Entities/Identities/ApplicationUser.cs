c1123ad3af0cef1228d6c5f0bc8155a06c37ec50
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:43:53 +0300

    #6.2 Add pointer in entities

--- WatchShop_Core/Domain/Entities/Identities/ApplicationUser.cs
+++ WatchShop_Core/Domain/Entities/Identities/ApplicationUser.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WatchShop_Core.Domain.Contracts;
 
 namespace WatchShop_Core.Domain.Entities.Identities
 {
-    public class ApplicationUser : IdentityUser<Guid>
+    public class ApplicationUser : IdentityUser<Guid>, IEntity
     {
         [StringLength(30)]
         public string? City { get; set; }
