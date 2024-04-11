b8c71794e84df25d0a15bba4522b6c3ad55c338e
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:44:18 +0300

    #6.3 Create custom order argument exception

---
+++ WatchShop_Core/Exceptions/OrderArgumentException.cs
@@ -0,0 +1,11 @@
+﻿namespace WatchShop_Core.Exceptions
+{
+    public class OrderArgumentException : Exception
+    {
+        public OrderArgumentException() : base() { }
+
+        public OrderArgumentException(string message) : base(message) { }
+
+        public OrderArgumentException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
