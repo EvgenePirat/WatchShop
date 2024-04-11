92cccc2ff4374d9c782ea1a87f71d447848c9597
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:18:05 +0300

    #5.1 Create models for working with jwt authentification

---
+++ WatchShop_Core/Models/Auth/Request/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WatchShop_Core.Models.Auth.Request
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ WatchShop_Core/Models/Auth/Response/AuthResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchShop_Core.Models.Auth.Response
+{
+    public class AuthResponseModel
+    {
+        public Guid UserId { get; set; }
+
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
