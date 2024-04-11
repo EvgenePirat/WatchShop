fc60a9f07ca809bcfd3c6cf8b76e726ef2836c32
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:49:38 +0300

    #6.7 Create mapper for working with order models in core

---
+++ WatchShop_Core/Mappers/CartModelProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using WatchShop_Core.Domain.Entities;
+using WatchShop_Core.Models.Carts.Request;
+using WatchShop_Core.Models.Carts.Response;
+
+namespace WatchShop_Core.Mappers
+{
+    public class CartModelProfile : Profile
+    {
+        public CartModelProfile()
+        {
+            CreateMap<CreateCartModel, Cart>();
+            CreateMap<UpdateCartModel, Cart>();
+            CreateMap<Cart, CartModel>();
+        }
+    }
+}
