63ff539d723199e8e2edbfdf152e924bdcc5d377
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:28:35 +0300

    #5.5 Create dtos for working with auth logic

---
+++ WatchShop/Dtos/Auth/Response/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WatchShop_UI.Dtos.Auth.Response
+{
+    public class AuthResponseDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
