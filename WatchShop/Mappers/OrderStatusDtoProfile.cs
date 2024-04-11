dd28b7490f71f5e1a5dd92f86401fc08683d5ea1
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-11 19:51:31 +0300

    #6.10 Create mappers for working with order in presentation layer

---
+++ WatchShop/Mappers/OrderStatusDtoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WatchShop_Core.Models.OrderStatuses.Response;
+using WatchShop_UI.Dtos.OrderStatuses.Response;
+
+namespace WatchShop_UI.Mappers
+{
+    public class OrderStatusDtoProfile : Profile
+    {
+        public OrderStatusDtoProfile()
+        {
+            CreateMap<OrderStatusModel, OrderStatusDto>();
+        }
+    }
+}
