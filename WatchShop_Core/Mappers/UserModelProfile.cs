95a98d60698f4bb63d256f120cad563332a8937b
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-12 15:55:47 -0700

    #6.7 Create models and mapper for working with user in core

---
+++ WatchShop_Core/Mappers/UserModelProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WatchShop_Core.Domain.Entities.Identities;
+using WatchShop_Core.Models.Users.Request;
+using WatchShop_Core.Models.Users.Response;
+
+namespace WatchShop_Core.Mappers
+{
+    public class UserModelProfile : Profile
+    {
+        public UserModelProfile()
+        {
+            CreateMap<UpdateUserModel, ApplicationUser>();
+            CreateMap<ApplicationUser, UserModel>();
+        }
+    }
+}
---
+++ WatchShop_Core/Models/Users/Response/UserModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WatchShop_Core.Models.Users.Response
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? City { get; set; }
+
+        public string Role { get; set; }
+    }
+}
