aa8c41a4fc5bdfd34dbedcd15290c571a53ffb9a
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-12 15:55:47 -0700

    #6.7 Create models and mapper for working with user in core

---
+++ WatchShop_Core/Models/Users/Request/UpdateUserModel.cs
@@ -0,0 +1,17 @@
+﻿using WatchShop_Core.Domain.Enums;
+
+namespace WatchShop_Core.Models.Users.Request
+{
+    public class UpdateUserModel
+    {
+        public string? FullName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? City { get; set; }
+
+        public RoleEnum Role { get; set; }
+    }
+}
