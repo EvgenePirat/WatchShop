fefe6cd6aab50b2d23c4a6acaced9dde4ad68dd1
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-12 15:56:31 -0700

    #6.8 Create user service cotract and implementation and registration user service in DI

---
+++ WatchShop_Core/ServiceContracts/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using WatchShop_Core.Models.Brends.Request;
+using WatchShop_Core.Models.Brends.Response;
+using WatchShop_Core.Models.Users.Request;
+using WatchShop_Core.Models.Users.Response;
+
+namespace WatchShop_Core.ServiceContracts
+{
+    public interface IUserService
+    {
+        Task<UserModel> UpdateUserAsync(Guid id, UpdateUserModel model);
+        Task<IEnumerable<UserModel>> GetAllUsersAsync();
+        Task DeleteUserAsync(Guid id);
+        Task<UserModel> GetUserByUserNameAsync(string username);
+        Task<UserModel> GetUserByIdAsync(Guid id);
+    }
+}
