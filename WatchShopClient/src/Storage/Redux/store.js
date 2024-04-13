d7c9d54b88ff99ebcbcdf6e09e3e9d816cb5ad49
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-13 11:25:24 +0300

    #Create watch slice store

--- WatchShopClient/src/Storage/Redux/store.js
+++ WatchShopClient/src/Storage/Redux/store.js
@@ -3,10 +3,12 @@ import brendApi from "../../apis/admin/brendApi";
 import watchApi from "../../apis/admin/watchApi";
 import userApi from "../../apis/admin/userApi";
 import authApi from "../../apis/admin/authApi";
+import { watchItemReducer } from "./Slices/watchItemClise";
 
 
 const store = configureStore({
     reducer:{
+        watchItemsStore : watchItemReducer,
         [brendApi.reducerPath] : brendApi.reducer,
         [watchApi.reducerPath] : watchApi.reducer,
         [userApi.reducerPath] : userApi.reducer,
