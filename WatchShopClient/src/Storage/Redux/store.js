dc018c3954e71db2ab30662d3f500f7963a4d48f
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-16 23:09:50 -0700

    #Create commentApi in redux query

--- WatchShopClient/src/Storage/Redux/store.js
+++ WatchShopClient/src/Storage/Redux/store.js
@@ -4,6 +4,7 @@ import watchApi from "../../apis/admin/watchApi";
 import userApi from "../../apis/admin/userApi";
 import authApi from "../../apis/admin/authApi";
 import { watchItemReducer } from "./Slices/watchItemClise";
+import commentApi from "../../apis/admin/commentApi";
 
 
 const store = configureStore({
@@ -12,9 +13,10 @@ const store = configureStore({
         [brendApi.reducerPath] : brendApi.reducer,
         [watchApi.reducerPath] : watchApi.reducer,
         [userApi.reducerPath] : userApi.reducer,
-        [authApi.reducerPath] : authApi.reducer
+        [authApi.reducerPath] : authApi.reducer,
+        [commentApi.reducerPath] : commentApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(brendApi.middleware).concat(watchApi.middleware).concat(userApi.middleware).concat(authApi.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(brendApi.middleware).concat(watchApi.middleware).concat(userApi.middleware).concat(authApi.middleware).concat(commentApi.middleware)
 });
 
 export default store;
