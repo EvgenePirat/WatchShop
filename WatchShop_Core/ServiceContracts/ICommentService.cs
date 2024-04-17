91fde3f4a2f14216fb9328244f0b4705315beaee
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-16 22:12:05 -0700

    #7.5 Create comment service contract and realize crud operations

---
+++ WatchShop_Core/ServiceContracts/ICommentService.cs
@@ -0,0 +1,27 @@
+﻿using WatchShop_Core.Models.Brends.Request;
+using WatchShop_Core.Models.Brends.Response;
+using WatchShop_Core.Models.Comments.Request;
+using WatchShop_Core.Models.Comments.Response;
+using WatchShop_Core.Models.Orders.Request;
+using WatchShop_Core.Models.Orders.Response;
+using WatchShop_Core.Models.Watches.Response;
+
+namespace WatchShop_Core.ServiceContracts
+{
+    public interface ICommentService
+    {
+        Task<CommentModel> CreateCommentAsync(CreateCommentModel model);
+
+        Task<IEnumerable<CommentModel>> GetAllCommentsAsync();
+
+        Task DeleteCommentAsync(Guid id);
+
+        Task<CommentModel> UpdateCommentAsync(Guid id, UpdateCommentModel model);
+
+        Task<IEnumerable<CommentModel>> GetCommentsByUserNameAsync(string username);
+
+        Task<IEnumerable<CommentModel>> GetCommentsByWatchNameModelAsync(string nameModel);
+
+        Task<CommentModel> GetCommentByIdAsync(Guid id);
+    }
+}
---
+++ WatchShop_Core/Services/CommentService.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using WatchShop_Core.Domain.Entities;
+using WatchShop_Core.Domain.RepositoryContracts;
+using WatchShop_Core.Exceptions;
+using WatchShop_Core.Models.Comments.Request;
+using WatchShop_Core.Models.Comments.Response;
+using WatchShop_Core.ServiceContracts;
+
+namespace WatchShop_Core.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CommentService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<CommentModel> CreateCommentAsync(CreateCommentModel model)
+        {
+            var commentToCreate = _mapper.Map<WatchComment>(model);
+
+            _unitOfWork.WatchCommentRepository.Add(commentToCreate);
+
+            await _unitOfWork.SaveAsync();
+
+            return _mapper.Map<CommentModel>(commentToCreate);
+        }
+
+        public async Task DeleteCommentAsync(Guid id)
+        {
+            var commentToDelete = await _unitOfWork.WatchCommentRepository.GetByIdAsync(id);
+
+            if (commentToDelete == null)
+                throw new CommentWatchArgumentException($"Comment by id {id} not found for delete");
+
+            _unitOfWork.WatchCommentRepository.Delete(commentToDelete);
+
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task<IEnumerable<CommentModel>> GetAllCommentsAsync()
+        {
+            var comments = await _unitOfWork.WatchCommentRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<CommentModel>>(comments);
+        }
+
+        public async Task<CommentModel> GetCommentByIdAsync(Guid id)
+        {
+            var comment = await _unitOfWork.WatchCommentRepository.GetByIdAsync(id);
+
+            if (comment == null)
+                throw new CommentWatchArgumentException($"Comment by id {id} not found for delete");
+
+            return _mapper.Map<CommentModel>(comment);
+        }
+
+        public async Task<IEnumerable<CommentModel>> GetCommentsByUserNameAsync(string username)
+        {
+            var comments = await _unitOfWork.WatchCommentRepository.FindCommentByUserNameAsync(username);
+
+            if(comments.Count() == 0)
+            {
+                throw new CommentWatchArgumentException($"Comments by username not found");
+            }
+
+            return _mapper.Map<IEnumerable<CommentModel>>(comments);
+        }
+
+        public async Task<IEnumerable<CommentModel>> GetCommentsByWatchNameModelAsync(string nameModel)
+        {
+            var comments = await _unitOfWork.WatchCommentRepository.FindCommentByWatchNameModelAsync(nameModel);
+
+            if (comments.Count() == 0)
+            {
+                throw new CommentWatchArgumentException($"Comments by watch nameModel {nameModel} not found");
+            }
+
+            return _mapper.Map<IEnumerable<CommentModel>>(comments);
+        }
+
+        public async Task<CommentModel> UpdateCommentAsync(Guid id, UpdateCommentModel model)
+        {
+            var commentToUpdate = await _unitOfWork.WatchCommentRepository.GetByIdAsync(id);
+
+            if (commentToUpdate == null)
+                throw new CommentWatchArgumentException($"Comment by id {id} not found for delete");
+
+            commentToUpdate.Comment = model.Comment;
+
+            _unitOfWork.WatchCommentRepository.Update(commentToUpdate);
+
+            await _unitOfWork.SaveAsync();
+
+            return _mapper.Map<CommentModel>(commentToUpdate);
+        }
+    }
+}
