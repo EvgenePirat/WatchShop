150c99a5b2df1b1a90855ac2ce56276964597ff2
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-22 14:04:32 -0700

    #10.14 Create migration and update database with new field

---
+++ WatchShop_Infrastructure/Migrations/20240422191116_addNewFieldInPayment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchShop_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewFieldInPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "payments");
+        }
+    }
+}
