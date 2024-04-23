804f8c6e4f921e90db5bfb6bb1d202d4b1da31ee
eugene.brexyn <eugene.brexyn@gmail.com>
2024-04-23 11:01:43 +0300

    #10.21 Create model and dto for working with message

---
+++ WatchShop/Dtos/Messages/Request/PostMessageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace WatchShop_UI.Dtos.Messages.Request
+{
+    public class PostMessageDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Message { get; set; }
+    }
+}
