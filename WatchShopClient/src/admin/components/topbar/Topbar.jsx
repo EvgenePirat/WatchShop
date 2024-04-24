7915793cfa820ae98b4113a626586bc6a1c09242
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-24 14:04:38 -0700

    #Show username for admin panel who use

--- WatchShopClient/src/admin/components/topbar/Topbar.jsx
+++ WatchShopClient/src/admin/components/topbar/Topbar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './topbar.css'
 import HomeIcon from '@mui/icons-material/Home';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Topbar() {
+
+    const userAuth = useSelector(state => state.userAuthStore);
+
   return (
     <div className='topbar'>
         <div className='topbarWrapper'>
@@ -13,6 +17,9 @@ function Topbar() {
             <div className="topRight">
                 <div className="topbarIconContainer">
                     <Link to="/"><HomeIcon /></Link>
+                </div>
+                <div >
+                    {userAuth.username}
                 </div>
                 <img src='https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/ijsi5fzb1nbkbhxa2gc1.png' alt='' className='topAvatar' />
             </div>
