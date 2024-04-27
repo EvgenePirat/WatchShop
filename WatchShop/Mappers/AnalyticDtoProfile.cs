d19aa92834730fac2a46ee1e83969176bfb94546
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-27 13:47:14 -0700

    #12.3 Create and registration mapper for working with model to dto for analytic logic

---
+++ WatchShop/Mappers/AnalyticDtoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WatchShop_Core.Models.SalesStatistics.Response;
+using WatchShop_UI.Dtos.SalesStatistics.Response;
+
+namespace WatchShop_UI.Mappers
+{
+    public class AnalyticDtoProfile : Profile
+    {
+        public AnalyticDtoProfile()
+        {
+            CreateMap<SalesStatisticModel, SalesStatisticDto>();
+        }
+    }
+}
