d5acc002492f8304bc82fa6e56c9379afd30b565
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-04-27 13:47:43 -0700

    #12.4 Create controller with method for get basic statistics

---
+++ WatchShop/Controllers/AnalyticController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WatchShop_Core.ServiceContracts;
+using WatchShop_Core.Services;
+using WatchShop_UI.Dtos.Brends.Response;
+using WatchShop_UI.Dtos.SalesStatistics.Response;
+using WatchShop_UI.Utilities.GeneralResponse;
+
+namespace WatchShop_UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalyticController : ControllerBase
+    {
+        private readonly IAnalyticService _analyticService;
+        private readonly IMapper _mapper;
+        private readonly ILogger<AnalyticController> _logger;
+
+        public AnalyticController(IAnalyticService analyticService, IMapper mapper, ILogger<AnalyticController> logger)
+        {
+            _analyticService = analyticService;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetSalesStatisticsAsync()
+        {
+            _logger.LogInformation("{controller}.{method} - Get, get sales statistic, Task started",
+                nameof(AnalyticController), nameof(GetSalesStatisticsAsync));
+
+            var result = await _analyticService.CalculateSalesStatisticsAsync();
+
+            var mappedResult = _mapper.Map<SalesStatisticDto>(result);
+
+            ApiResponse response = new ApiResponse()
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                Result = mappedResult
+            };
+
+            _logger.LogInformation("{controller}.{method} - Get, get sales statistic, Result - Ok, Task ended",
+                nameof(AnalyticController), nameof(GetSalesStatisticsAsync));
+
+            return Ok(response);
+        }
+    }
+}
